45025263ffc963edbaed8fe5fad01e06808af6da
yamazoetakahiro3-boop <yamazoe.takahiro3@gmail.com>
2026-08-13 10:27:56 +0900

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='s5c-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.json']))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
